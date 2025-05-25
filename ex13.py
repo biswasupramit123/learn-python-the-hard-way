aac99ab1e089b3e27e7b4bb63f2ba66189f73c68
biswasupramit123 <biswasupramit123@gmail.com>
2025-05-25 09:25:55 +0530

    Initial commit

---
+++ ex13.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print("This script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third) 
+print("Your fifth variable is:", input("Enter your fifth variable: "))
+print("Your sixth variable is:", input("Enter your sixth variable: "))
